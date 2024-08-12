00dae0649a216b5eb26e98804026d3660fe1b715
Nandezu <info@nandezu.com>
2024-08-12 18:00:28 +0200

    Zjednodušení metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -3,9 +3,4 @@ const { getDefaultConfig } = require('expo/metro-config');
 const defaultConfig = getDefaultConfig(__dirname);
 defaultConfig.resolver.sourceExts.push('cjs');
 
-// Přidáme konfiguraci pro SVG, pokud ji používáte
-defaultConfig.transformer.babelTransformerPath = require.resolve("react-native-svg-transformer");
-defaultConfig.resolver.assetExts = defaultConfig.resolver.assetExts.filter((ext) => ext !== "svg");
-defaultConfig.resolver.sourceExts.push("svg");
-
 module.exports = defaultConfig;
